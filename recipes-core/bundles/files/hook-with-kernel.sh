94260c806d0b04241c68b5643c303f80d89262ef
Pierrick Curt <pierrickcurt@gmail.com>
2024-03-14 14:21:11 +0000

    feat(bundles): backup ssh authorized_keys during OTA updates

--- recipes-core/bundles/files/hook-with-kernel.sh
+++ recipes-core/bundles/files/hook-with-kernel.sh
@@ -7,6 +7,8 @@ case "$1" in
                         # Backup WIFI cretentials
                         cp /etc/wpa_supplicant/wpa_supplicant-wlan0.conf /tmp/
                 fi
+                # Backup .ssh/authorized_keys
+                cp -f /home/root/.ssh/authorized_keys /tmp/ 2>/dev/null
                 ;;
         slot-post-install)
                 # Check if WIFI cretentials have been backuped
@@ -14,6 +16,12 @@ case "$1" in
                         # Restore
                         cp /tmp/wpa_supplicant-wlan0.conf "$RAUC_SLOT_MOUNT_POINT"/etc/wpa_supplicant/
                         ln -s "$RAUC_SLOT_MOUNT_POINT"/lib/systemd/system/wpa_supplicant@.service "$RAUC_SLOT_MOUNT_POINT"/etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlan0.service
+                fi
+                # Check if .ssh/authorized_keys have been backuped
+                if [ -e "/tmp/authorized_keys" ]; then
+                        # Restore
+                        mkdir "$RAUC_SLOT_MOUNT_POINT"/home/root/.ssh
+                        cp /tmp/authorized_keys "$RAUC_SLOT_MOUNT_POINT"/home/root/.ssh
                 fi
                 # Update kernel, devicetree and bootscript
                 cp "$RAUC_BUNDLE_MOUNT_POINT"/Image /boot/Image
